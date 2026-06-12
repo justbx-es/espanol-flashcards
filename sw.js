2a6b8f90b7e8396483d7df0cf64419ae0e0afcb3
justbx-es <julienstebeyz@gmail.com>
2026-06-12 23:12:15 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'espanol-v1';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchAndCache = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      });
+      return cached || fetchAndCache;
+    })
+  );
+});
